f941521e2c8c558500f9adcdca8a294c924df248
Elijah <elqtn.wrk@gmail.com>
2024-01-30 13:58:33 +0800

    create index, and other webpages

---
+++ test.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!-- write test php code -->
+    <h1>test</h1>
+    <?php
+    $number1 = 5;
+    $number2 = 10;
+    $sum = $number1 + $number2;
+    echo "The sum of $number1 and $number2 is: $sum";
+    ?>
+</body>
+</html>
